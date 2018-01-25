4ac96288dda2fb495b7ee75a05e6e9a61e2f8821
sethlan <sethlans@protonmail.com>
2018-01-25 14:45:46 +0800

    add brain-calc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import { welcome,getUserName,questionEven } from './stdFunc';
+import { cons, car, cdr } from 'hexlet-pairs';
+import { welcome, getUserName, question, randomNumber } from './stdFunc';
 
+export const brainGames = () => {
+  welcome('');
+  getUserName();
+};
 export const brainEven = () => {
   welcome('Answer "yes" if number even otherwise answer "no".');
   const userName = getUserName();
+
   let count = 1;
   while (count <= 3) {
-  // for (let i = 1; i <= 3; i += 1) {
-    if (questionEven(userName)) {
+    const number = randomNumber();
+    if (question(userName, number, numbers => (numbers % 2 === 0 ? 'yes' : 'no'), number)) {
       count += 1;
     } else {
       break;
@@ -16,11 +22,20 @@ export const brainEven = () => {
     console.log(`Congratulations, ${userName}!`);
   }
 };
-export const brainGames = () => {
-  welcome();
-  getUserName();
-};
 export const brainCalc = () => {
   welcome('What is the result of the expression?');
   const userName = getUserName();
+  let numb1 = randomNumber();
+  let numb2 = randomNumber();
+  if (question(userName, `${numb1} + ${numb2}`, numbers => String(car(numbers) + cdr(numbers)), cons(numb1, numb2))) {
+    numb1 = randomNumber();
+    numb2 = randomNumber();
+    if (question(userName, `${numb1} * ${numb2}`, numbers => String(car(numbers) * cdr(numbers)), cons(numb1, numb2))) {
+      numb1 = randomNumber();
+      numb2 = randomNumber();
+      if (question(userName, `${numb1} - ${numb2}`, numbers => String(car(numbers) - cdr(numbers)), cons(numb1, numb2))) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    }
+  }
 };
--- src/stdFunc.js
+++ src/stdFunc.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
-import { cons, car, cdr, toString } from 'hexlet-pairs';
 
 export const welcome = (ruleOfGame) => {
   console.log('Welcome to the Brain Games!');
-  console.log(ruleOfGame);
+  if (ruleOfGame !== '') { console.log(ruleOfGame); }
 };
 export const getUserName = () => {
   const name = readlineSync.question('May I have your name?');
@@ -11,11 +10,35 @@ export const getUserName = () => {
   return name;
 };
 export const getAnswer = () => readlineSync.question('Your answer:');
-export const question = (whatAsking) => {
+export const question = (namePlayer, whatAsking, func, numbers) => {
   console.log(`Question:${whatAsking}`);
-  return getAnswer();
+  const answer = getAnswer();
+  const trueAnswer = func(numbers);
+  if (answer === trueAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+  console.log(`Let's try again, ${namePlayer}!`);
+  return false;
 };
 export const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+/*
+export const repetion = (howManyTimes, namePlayer, whatAsking, func, numbers) => {
+  let count = 1;
+  while (count <= howManyTimes) {
+  // for (let i = 1; i <= 3; i += 1) {
+    if (question(namePlayer, whatAsking, func, numbers)) {
+      count += 1;
+    } else {
+      break;
+    }
+  }
+  if (count === howManyTimes + 1) {
+    console.log(`Congratulations, ${namePlayer}!`);
+  }
+};
+/*
 export const questionEven = (namePlayer) => {
   const numb = randomNumber;
   const answer = question(numb);
@@ -32,5 +55,5 @@ export const questionEven = (namePlayer) => {
 export const questionCalc = (namePlayer, expression) => {
   const numb1 = randomNumber;
   const numb2 = randomNumber;
-
-}
+};
+*/
